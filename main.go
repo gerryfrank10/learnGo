d8c48ae504ce44937d5369e12e1439d537fd543c
gerald <amasi.gerald@icloud>
2025-03-08 16:23:04 +0000

    Functions, Loops and Conditions

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 const PI = 3.1415926
 
@@ -35,7 +38,7 @@ func main() {
 	arr3 := [5]int{1: 10, 2: 40}
 
 	// arrays and slice
-	myslice1 := make([]int, 5, 10)
+	myslice1 := make([]int, 5, 10) //slice [type, length, capacity]
 
 	fmt.Println(arr1)
 	fmt.Println(arr2)
@@ -48,4 +51,62 @@ func main() {
 	fmt.Printf("myslice1 = %v\n", myslice1)
 	fmt.Printf("Length = %d\n", len(myslice1))
 	fmt.Printf("Cap = %d\n", cap(myslice1))
+
+	fmt.Println("------------------")
+	//Appending slices
+	myslice2 := []int{1, 2, 3}
+	myslice3 := []int{10, 20, 30}
+	myslice4 := append(myslice2, myslice3...)
+	fmt.Printf("myslice4 = %v\n", myslice4)
+
+	// Operators are quite similar to other
+	// Conditions
+	time := 20
+	if time < 18 {
+		fmt.Println("Good Day")
+	} else {
+		fmt.Println("Good Evening")
+	}
+	// Loops
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+	//Looping in arrays
+	for idx, val := range cars {
+		fmt.Printf("%v\t%v\n", idx, val)
+	}
+	// Or
+	fruits := []string{"apple", "mango", "peach", "pear"}
+	for _, fruit := range fruits {
+		if fruit == "peach" {
+			fmt.Println("I am peach")
+		} else {
+			fmt.Println("I am not peach")
+		}
+	}
+	Message("Gerald")
+
+	pew := addition(1, 2)
+	fmt.Println(pew)
+
+	// Get addition and the length of the number
+	result, length := myFunc(20, 40)
+	fmt.Printf("myFunc addition is %v and length is %v\n", result, length)
+}
+
+// Message Functions
+func Message(fname string) {
+	fmt.Printf("Hello, %s!\n", fname)
+}
+
+// Function returning value
+func addition(a, b int) int {
+	return a + b
+}
+
+// Function with multiple returns
+func myFunc(x int, y int) (result int, length int) {
+	result = x + y
+	length = len(strconv.Itoa(result))
+	return
 }
